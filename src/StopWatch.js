3207bcbb99ebb0c1e8c2add37f19dfc93304a594
Joaquin Cid <joaquin@weareadaptive.com>
2020-06-10 18:11:24 +0200

    amend bugs

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,6 +3,7 @@ class StopWatch {
 
   start() {
     this.timeStarted = Date.now();
+    console.log("start: " + this.timeStarted);
   }
   
   stop() {
@@ -11,14 +12,27 @@ class StopWatch {
     this.minLapRecord = null;
     this.maxLapRecord = null;
     this.sumOfLapTimes = 0;
+    this.elapsedTimeWhenPaused = null;
+    console.log("stop: " + this.timeStarted);
   }
   
   pause() {
-  
+    if (this.timeStarted === null) return;
+
+    this.elapsedTimeWhenPaused = this.elapsedTime();
+    console.log("pause: " + this.timeStarted);
   }
 
   resume() {
-    this.timeStarted = Date.now() - this.timeStarted;
+    if (this.elapsedTimeWhenPaused === null) return;
+
+    console.log("is called resume: " + this.timeStarted);
+    console.log("         dateNow: " + Date.now());
+
+    this.timeStarted = Date.now() - this.elapsedTimeWhenPaused;
+
+    this.elapsedTimeWhenPaused = null;
+    console.log("resume: " + this.timeStarted);
   }
   
   elapsedTime() {
@@ -28,13 +42,15 @@ class StopWatch {
 
   newLap() {
     if (this.timeStarted === null) return;
+    if (this.elapsedTimeWhenPaused === null) return;
 
-    let newRecord = (this.laps.length > 0) ? this.elapsedTime() - sumOfLapTimes : this.elapsedTime();
+    let newRecord = (this.laps.length > 0) ? this.elapsedTime() - this.sumOfLapTimes : this.elapsedTime();
     this.laps.push(newRecord);
     this.sumOfLapTimes += newRecord;
 
     if (this.minLapRecord === null || this.minLapRecord > newRecord) this.minLapRecord = newRecord;
     if (this.maxLapRecord < newRecord) this.maxLapRecord = newRecord;
+    console.log("newLap: " + this.timeStarted);
   }
   
   laps() {
@@ -43,13 +59,10 @@ class StopWatch {
 
   minLap() {
     return this.minLapRecord;
-    return  Math.min(...this.laps);
   }
   
   maxLap() {
     return this.maxLapRecord;
-
-    return  Math.max(...this.laps);
   }
 }
 
